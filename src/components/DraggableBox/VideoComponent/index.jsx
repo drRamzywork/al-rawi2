918f95d994811ca720f1693903a66a7883410625
moelshenawy <mohamedgk5@gmail.com>
2024-10-27 22:09:05 +0300

    initial commit

---
+++ src/components/DraggableBox/VideoComponent/index.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useRef } from 'react'
+import styles from './index.module.scss';
+
+const VideoComponent = ({
+  currentVideo,
+  muted,
+  setMuted,
+  isClosed,
+  isMenu,
+  setIsMenu
+  , }) => {
+  const videoRef = useRef(null);
+
+  const handleVideoEnd = () => {
+    // setIsClosed(false);
+    setIsMenu(true);
+    videoRef.current.pause();
+
+  };
+  const handleUnmute = () => {
+    if (videoRef.current) {
+      setMuted(false)
+
+      videoRef.current.play();
+      if (muted === true) {
+        // videoRef.current.muted = false;
+      }
+
+    }
+  };
+
+
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.muted = muted;
+      videoRef.current.play().catch((error) => {
+        console.error('Autoplay failed:', error);
+      });
+
+
+
+      // if (isClosed === false) {
+      //   videoRef.current.pause();
+      // }
+
+      //   videoRef.current.pause();
+      // }
+
+
+      if (isMenu === true) {
+        videoRef.current.pause();
+      }
+
+
+    }
+
+  }, [currentVideo, muted, isClosed,]);
+
+
+
+
+
+  return (
+    <>
+      <div className={styles.video_container} onClick={handleUnmute}>
+        <video
+          key={currentVideo}
+          ref={videoRef}
+          className={styles.videoElement}
+          src={currentVideo}
+          controls
+          autoPlay
+          playsInline
+          onEnded={handleVideoEnd}
+        />
+      </div>
+
+    </>
+  )
+}
+
+export default VideoComponent
+
+
+
+// +++++++++++++++++++++++++++++++
